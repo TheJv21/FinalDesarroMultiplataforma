4e858bc0455a8757027a949aba63fc2aa1de47db
Jesse <jessealas6@gmail.com>
2023-05-22 12:52:59 +0000

    Base de datos final

--- Database/database.sql
+++ Database/database.sql
@@ -47,9 +47,159 @@ GO
 ALTER TABLE universidad.dbo.asignacion ADD CONSTRAINT fk_asignacion_estudiante FOREIGN KEY ( carnet ) REFERENCES universidad.dbo.estudiante( carnet );
 GO
 
+CREATE PROCEDURE eliminar_asignacion
+    @id_asignacion INT
+AS
+BEGIN
+    DELETE FROM asignacion
+    WHERE id_asignacion = @id_asignacion;
+END;;
+GO
+
+CREATE PROCEDURE eliminar_curso
+    @id_curso INT
+AS
+BEGIN
+    DELETE FROM curso
+    WHERE id_curso = @id_curso;
+END;;
+GO
+
+CREATE PROCEDURE eliminar_estudiante
+    @carnet NVARCHAR(50)
+AS
+BEGIN
+    DELETE FROM estudiante
+    WHERE carnet = @carnet;
+END;;
+GO
+
+CREATE PROCEDURE listar_asignacion
+AS
+BEGIN
+    SELECT * FROM asignacion;
+END;;
+GO
+
+CREATE PROCEDURE listar_curso
+AS
+BEGIN
+    SELECT * FROM curso;
+END;;
+GO
+
+CREATE PROCEDURE listar_estudiante
+AS
+BEGIN
+    SELECT * FROM estudiante;
+END;;
+GO
+
+CREATE PROCEDURE modificar_asignacion
+    @id_asignacion INT,
+    @carnet NVARCHAR(50),
+    @id_curso INT,
+    @seccion NVARCHAR(50),
+    @fecha_realizacion DATE,
+    @semestre NVARCHAR(50),
+    @anho NVARCHAR(10)
+AS
+BEGIN
+    UPDATE asignacion
+    SET carnet = @carnet,
+        id_curso = @id_curso,
+        seccion = @seccion,
+        fecha_realizacion = @fecha_realizacion,
+        semestre = @semestre,
+        anho = @anho
+    WHERE id_asignacion = @id_asignacion;
+END;;
+GO
+
+CREATE PROCEDURE modificar_curso
+    @id_curso INT,
+    @nombre NVARCHAR(MAX),
+    @semestre NVARCHAR(50),
+    @valor_creditos NVARCHAR(50)
+AS
+BEGIN
+    UPDATE curso
+    SET nombre = @nombre,
+        semestre = @semestre,
+        valor_creditos = @valor_creditos
+    WHERE id_curso = @id_curso;
+END;;
+GO
+
+CREATE PROCEDURE modificar_estudiante
+    @carnet NVARCHAR(50),
+    @nombres NVARCHAR(MAX),
+    @apellidos NVARCHAR(MAX),
+    @carrera NVARCHAR(100),
+    @correo NVARCHAR(100),
+    @telefono NVARCHAR(50),
+    @genero NVARCHAR(50),
+    @fecha_ingreso DATE
+AS
+BEGIN
+    UPDATE estudiante
+    SET nombres = @nombres,
+        apellidos = @apellidos,
+        carrera = @carrera,
+        correo = @correo,
+        telefono = @telefono,
+        genero = @genero,
+        fecha_ingreso = @fecha_ingreso
+    WHERE carnet = @carnet;
+END;;
+GO
+
+CREATE PROCEDURE registrar_asignacion
+    @carnet NVARCHAR(50),
+    @id_curso INT,
+    @seccion NVARCHAR(50),
+    @fecha_realizacion DATE,
+    @semestre NVARCHAR(50),
+    @anho NVARCHAR(10)
+AS
+BEGIN
+    INSERT INTO asignacion (carnet, id_curso, seccion, fecha_realizacion, semestre, anho)
+    VALUES (@carnet, @id_curso, @seccion, @fecha_realizacion, @semestre, @anho);
+END;;
+GO
+
+CREATE PROCEDURE registrar_curso
+    @id_curso INT,
+    @nombre NVARCHAR(MAX),
+    @semestre NVARCHAR(50),
+    @valor_creditos NVARCHAR(50)
+AS
+BEGIN
+    INSERT INTO curso (id_curso, nombre, semestre, valor_creditos)
+    VALUES (@id_curso, @nombre, @semestre, @valor_creditos);
+END;;
+GO
+
+CREATE PROCEDURE registrar_estudiante
+    @carnet NVARCHAR(50),
+    @nombres NVARCHAR(MAX),
+    @apellidos NVARCHAR(MAX),
+    @carrera NVARCHAR(100),
+    @correo NVARCHAR(100),
+    @telefono NVARCHAR(50),
+    @genero NVARCHAR(50),
+    @fecha_ingreso DATE
+AS
+BEGIN
+    INSERT INTO estudiante (carnet, nombres, apellidos, carrera, correo, telefono, genero, fecha_ingreso)
+    VALUES (@carnet, @nombres, @apellidos, @carrera, @correo, @telefono, @genero, @fecha_ingreso);
+END;;
+GO
+
 INSERT INTO universidad.dbo.curso( id_curso, nombre, semestre, valor_creditos ) VALUES ( 111, 'Anatomia', 'Tercero', '3');
 INSERT INTO universidad.dbo.curso( id_curso, nombre, semestre, valor_creditos ) VALUES ( 245, 'Matematicas', 'Tercero', '3');
 INSERT INTO universidad.dbo.curso( id_curso, nombre, semestre, valor_creditos ) VALUES ( 456, 'Biologia', 'Tercero', '3');
 INSERT INTO universidad.dbo.estudiante( carnet, nombres, apellidos, carrera, correo, telefono, genero, fecha_ingreso ) VALUES ( '201825167', 'Laura', 'Angelica', 'Enfermeria', 'laura@mail.edu', '45475234', 'Femenino', '2018-01-01');
 INSERT INTO universidad.dbo.estudiante( carnet, nombres, apellidos, carrera, correo, telefono, genero, fecha_ingreso ) VALUES ( '201825865', 'Susan', 'Regina', 'Enfermeria', 'susan@mail.edu', '45341234', 'Femenino', '2018-01-01');
 INSERT INTO universidad.dbo.estudiante( carnet, nombres, apellidos, carrera, correo, telefono, genero, fecha_ingreso ) VALUES ( '201845244', 'Maria', 'Antonieta', 'Enfermeria', 'maria@mail.edu', '41641234', 'Femenino', '2018-01-01');
+INSERT INTO universidad.dbo.asignacion( carnet, id_curso, seccion, fecha_realizacion, semestre, anho ) VALUES ( '201825865', 111, 'A', '2023-05-22', 'Tercero', '2023');
--- Database/insertar_datos.sql
+++
@@ -1,9 +0,0 @@
-
-INSERT [dbo].[estudiante] ([carnet], [nombres], [apellidos], [carrera], [correo], [telefono], [genero], [fecha_ingreso]) VALUES (201825865, N'Susan', N'Regina', N'Enfermeria', N'susan@mail.edu', N'45341234', N'Femenino', N'2018-01-01')
-INSERT [dbo].[estudiante] ([carnet], [nombres], [apellidos], [carrera], [correo], [telefono], [genero], [fecha_ingreso]) VALUES (201845244, N'Maria', N'Antonieta', N'Enfermeria', N'maria@mail.edu', N'41641234', N'Femenino', N'2018-01-01')
-INSERT [dbo].[estudiante] ([carnet], [nombres], [apellidos], [carrera], [correo], [telefono], [genero], [fecha_ingreso]) VALUES (201825167, N'Laura', N'Angelica', N'Enfermeria', N'laura@mail.edu', N'45475234', N'Femenino', N'2018-01-01')
-
-
-INSERT [dbo].[curso] ([id_curso], [nombre], [semestre], [valor_creditos]) VALUES (111, N'Anatomia', N'Tercero', N'3')
-INSERT [dbo].[curso] ([id_curso], [nombre], [semestre], [valor_creditos]) VALUES (245, N'Matematicas', N'Tercero', N'3')
-INSERT [dbo].[curso] ([id_curso], [nombre], [semestre], [valor_creditos]) VALUES (456, N'Biologia', N'Tercero', N'3')
